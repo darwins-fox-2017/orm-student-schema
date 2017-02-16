d79b91f842f97828bcfb4e7304ff11d837bdcd63
hepiska <ego1403@gmail.com>
2017-02-16 19:35:20 +0700

    student data sequelize done

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 "use strict"
+let  model = require('./models');
+
+model.Student.create({firstname:'bala',lastname:'bala', birthdate:'1992-04-12',email :'acot@gmail.com',height:151,phonenumber:'081239283192'}).
+then(function(){
+  console.log('data inserted');
+}).catch(function(err){
+  console.log('tinggi kurang/ email tidak sesuai / email sudah terdaftar / nomor hp kurang');
+})
+;
+//
+// model.Student.findOne({
+//   where : {firstname:'hepiska'}
+// }).then(function(hasil){
+//   console.log(hasil.getFullName());
+// })
+
+// model.Student.getAllData(function(callback){
+//   //console.log(callback);
+//   callback.forEach(function(hasil){
+//     console.log(hasil);
+//   })
+// });
+//
+// model.Student.getAgeData(function(result){
+//   console.log(result);
+// })
