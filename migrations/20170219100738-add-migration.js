74d94dab11a75d5ef625c08572a994008692decb
isumizumi <isumi.karina72@gmail.com>
2017-02-19 18:48:24 +0700

    Finish

---
+++ migrations/20170219100738-add-migration.js
@@ -0,0 +1,33 @@
+'use strict';
+
+module.exports = {
+  up: function (queryInterface, Sequelize) {
+    /*
+      Add altering commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.createTable('users', { id: Sequelize.INTEGER });
+    */
+    return queryInterface.addColumn('Students', 'email', Sequelize.STRING).then(function(){
+      queryInterface.addColumn('Students', 'height', Sequelize.INTEGER).then(function(){
+        queryInterface.addColumn('Students', 'phone', Sequelize.STRING)
+      })
+    })
+  },
+
+  down: function (queryInterface, Sequelize) {
+    /*
+      Add reverting commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.dropTable('users');
+    */
+    return queryInterface.removeColumn('Students', 'email').then(function(){
+      queryInterface.removeColumn('Students', 'height').then(function(){
+        queryInterface.removeColumn('Students', 'phone')
+      })
+    })
+  }
+};
